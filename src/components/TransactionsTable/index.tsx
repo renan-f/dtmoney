990de14a55ac874bde5d2604ba40bc4f219d0b78
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-09-02 21:35:54 -0300

    Criação de tabela de movimentações

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+
+                    <tr>
+                        <td>Curso rocketseat</td>
+                        <td className="withdraw">-R$7000</td>
+                        <td>Estudo</td>
+                        <td>25/08/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
