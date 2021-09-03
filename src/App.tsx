fb22b60d940cfa83940fbfb9c3b914669819e812
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-09-02 21:08:21 -0300

    Criação do componente Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global"
 
 export function App() {
   return (<>
     <Header />
+    <Dashboard />
     <GlobalStyles />
 
   </>);
