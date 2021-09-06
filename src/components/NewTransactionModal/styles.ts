e3ea21ec8ae76793be635220d9a821ee9e239ec3
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-09-06 15:31:13 -0300

    Adicionando funcionamento dos botões e funcionalidade de trocar o background do tipo que está selecionado

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -54,34 +54,39 @@ export const TransactionTypeContainer = styled.div`
     display:grid;
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
+`;
 
-    button {
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
 
-        background: transparent;
+interface RadioBoxProps {
+    isActive: boolean;
+};
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
 
-        display:flex;
-        align-items: center;
-        justify-content: center;
+    background: ${(props) => props.isActive ? '#eee' : 'transparent'};
 
-        transition: border-color 0.2s;
+    display:flex;
+    align-items: center;
+    justify-content: center;
 
-        &:hover{
-            border-color: ${darken('0.15', '#d7d7d7')};
-        }
+    transition: border-color 0.2s;
 
-        img { 
-            height:20px;
-            width: 20px;
-        }
-
-        span {
-            display: inline-block;
-            margin-left:1rem;
-            font-size:1rem;
-            color: var(--text-title);
-        }
+    &:hover{
+        border-color: ${darken('0.15', '#d7d7d7')};
     }
+
+    img { 
+        height:20px;
+        width: 20px;
+    }
+
+    span {
+        display: inline-block;
+        margin-left:1rem;
+        font-size:1rem;
+        color: var(--text-title);
+    }
+
 `;
