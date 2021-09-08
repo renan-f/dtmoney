39b5689287de43000b30f7ebb2b8ee57cca5edaa
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-09-08 18:42:01 -0300

    Carregando as transações para o context

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyles } from "./styles/global"
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -21,7 +21,7 @@ export function App() {
 
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -32,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyles />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
